c3c8e533bccebc5574ccff877d4b2b64b38c809e
root <root@LAPTOP-P1C25MSC.localdomain>
2026-02-09 09:04:44 +0700

    feat: restore and fix custom sign up page with full-page layout

--- app/Filament/Pages/Auth/CustomRegister.php
+++ app/Filament/Pages/Auth/CustomRegister.php
@@ -6,8 +6,23 @@ use Filament\Pages\Auth\Register;
 
 class CustomRegister extends Register
 {
+    protected static string $view = 'filament.pages.auth.custom-register';
+
+    public function render(): \Illuminate\Contracts\View\View
+    {
+        return view(static::$view, $this->getViewData())
+            ->layout('filament-panels::components.layout.base');
+    }
+
     protected function getRedirectUrl(): string
     {
         return '/dashboards/auth/login-success';
+    }
+
+    protected function getViewData(): array
+    {
+        return [
+            'loginUrl' => filament()->getLoginUrl(),
+        ];
     }
 }
